6c28bea977cd728628e5cb544f53ac70215dbf58
Ama Nkansah Akuoko <amankansahstate@gmail.com>
2025-06-23 11:36:20 +0000

    Add files via upload
    Added my nes JSX files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Ama Nkansah";
+const year = "2025";
+
+ReactDOM.render(
+  <>
+    <p>Created by {name}</p>
+    <p>Cpoyright {year}</p>
+  </>,
+  document.getElementById("root")
+);
